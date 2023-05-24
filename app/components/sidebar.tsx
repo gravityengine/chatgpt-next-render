00963b6a3f8ba761d8416411bc2e4ab033f4c7d3
admina1222 <76727661+admina1222@users.noreply.github.com>
2023-05-25 02:40:07 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
+            <a href="https://gravityengine.cc/archives/3256" target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
